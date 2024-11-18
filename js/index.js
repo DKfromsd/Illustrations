83b58822f7c5dfa79ca6495fd9be0eda6aa0c892
DoKyu Lee <dokyulee@Mac.lan>
2024-11-17 19:16:13 -0800

    hide-form

--- js/index.js
+++ js/index.js
@@ -38,29 +38,29 @@ const fillStaticText = () => {
   // getEl('js-feat3-desc').innerHTML = feat3Desc;
   // getEl('js-contact-desc').innerHTML = `If you want to experience our web services, please fill out the form below or contact us directly at <a href='mailto:${email}'>${email}</a> or ${phone}`;
 
-  getEl('js-contact-desc').innerHTML = `Getting started with Pen from the North West's Children's Books is easy. Simply sign up for our free consultation, and one of our experts will get in touch with you to discuss your specific needs and challenges.<br/> Together, we'll align illustration to your writings`;
+  getEl('js-contact-desc').innerHTML = `Getting started with Pen from the North West's Children's Books is easy. Please feel free to contact us directly at <a href='mailto:${email}'>${email}</a> Simply send questions for our free consultation, and one of our experts will get in touch with you to discuss your specific needs and challenges. Together, we'll align illustration to your writings.`;
 
-  getEl('js-contact-form').innerHTML = `<div class="contact-form-row">
-    <div class="contact-form-text">
-      <input type='off' class='form-textbox-input' id='name' name='name' autocomplete='nope' placeholder='Your name' required aria-label='Enter name'>
-    </div>
-    <div class='contact-form-sp'></div>
-    <div class="contact-form-text">
-      <input type='email' class='form-textbox-input' id='email' name='email' autocomplete='off' placeholder='Email address' required aria-label='Enter email'>
-    </div>
-    <div class='contact-form-sp'></div>
-    <div id='js-contact-select' class="contact-form-text form-dropdown dropdown-fader">
-      <select class="form-textbox-input form-dropdown-select" data-ignore-tracking="true" id="js-form-service" aria-labelledby="services-dropdown_label">
-        <option disabled selected="" value="">Select a service type</option>
-        <option class="services-dropdown-item" value="Starter Kit">Free Consulting</option>
-        <option class="services-dropdown-item" value="Consulting">Change Supports</option>
-        <option class="services-dropdown-item" value="Others">Others</option>
-      </select>
-    </div>
-  </div>
-  <textarea placeholder="Please briefly explain your needs." class="form-textbox-input contact-form-textarea" name="message" rows="10" required></textarea>
-  <button type='submit' class='button-blue'>Send your request</button>
-  `;
+  // getEl('js-contact-form').innerHTML = `<div class="contact-form-row">
+  //   <div class="contact-form-text">
+  //     <input type='off' class='form-textbox-input' id='name' name='name' autocomplete='nope' placeholder='Your name' required aria-label='Enter name'>
+  //   </div>
+  //   <div class='contact-form-sp'></div>
+  //   <div class="contact-form-text">
+  //     <input type='email' class='form-textbox-input' id='email' name='email' autocomplete='off' placeholder='Email address' required aria-label='Enter email'>
+  //   </div>
+  //   <div class='contact-form-sp'></div>
+  //   <div id='js-contact-select' class="contact-form-text form-dropdown dropdown-fader">
+  //     <select class="form-textbox-input form-dropdown-select" data-ignore-tracking="true" id="js-form-service" aria-labelledby="services-dropdown_label">
+  //       <option disabled selected="" value="">Select a service type</option>
+  //       <option class="services-dropdown-item" value="Starter Kit">Free Consulting</option>
+  //       <option class="services-dropdown-item" value="Consulting">Change Supports</option>
+  //       <option class="services-dropdown-item" value="Others">Others</option>
+  //     </select>
+  //   </div>
+  // </div>
+  // <textarea placeholder="Please briefly explain your needs." class="form-textbox-input contact-form-textarea" name="message" rows="10" required></textarea>
+  // <button type='submit' class='button-blue'>Send your request</button>
+  // `;
 
   getEl('js-footer').innerHTML = `© 2024 ${phrase1}`;
 }
