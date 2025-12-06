7ceb2ee8d2bf1d0d4dedc58f1ef9cb221f562cbb
DokyuLee <mickeyfromsd@gmail.com>
2025-12-05 21:01:40 -0800

    'fix-load-env'

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ let cn = 'tsohlacol';
 // console.log("cn: ", conv('')); // test cn
 
 const loadEnv = async () => {
-  //const res = await fetch('/env');
+  const res = await fetch('/env');
   if (!res.ok) {
     return;
   }
